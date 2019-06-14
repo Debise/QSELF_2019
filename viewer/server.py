af138fb440e2c3f152457c66621fd32d23cc7220
Pedro Costa <jules007.cesar@hotmail.com>
2019-06-14 10:01:49 +0200

    Viewer comparison is working

--- viewer/server.py
+++ viewer/server.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, jsonify
 import sys
 import os
 import glob
 sys.path.insert(0, '../')
 import settings as st
 from src.race_manager import RaceManager
+from src.race_inferer_wrapper import RaceInfererWrapper
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,6 +18,7 @@ app.config['JSON_SORT_KEYS'] = False
 # assets.register('main_js', js)
 
 RACE_MANAGER = RaceManager().load()
+RACE_INFERER = RaceInfererWrapper()
 
 
 @app.route('/', methods=['GET'])
@@ -49,6 +51,86 @@ def post_visualisation():
 def post_statistic():
     race_name = request.form['race_name']
     json_dict = RACE_MANAGER.races[race_name].get_statistics()
+
+    return jsonify(json_dict)
+
+
+@app.route('/race-detail/comparison_segments', methods=['POST'])
+def get_comparison_segments():
+    race_name = request.form['race_name']
+    deniv_segment, length_segment, density_segment = RACE_INFERER.get_best_segment(race_name)
+    segments = deniv_segment + length_segment + density_segment
+
+    json_dict = {}
+
+    for i, segment_tuple in enumerate(segments):
+        second_race = segment_tuple[0]
+        segment = segment_tuple[1]
+
+        json_dict[f'seg{i}'] = [second_race, segment.segment_type]
+
+    return jsonify(json_dict)
+
+
+@app.route('/race-detail/comparison_visualisation', methods=['POST'])
+def get_comparison_visualisation():
+    race_name = request.form['race_name']
+    comparison_race_name = request.form['comparison_race_name']
+    segment_type = request.form['segment_type']
+
+    output_folder = st.files["output_folder"]
+    filename = f"{race_name}_{comparison_race_name}_{segment_type}.html"
+    dir_filename = os.path.join(output_folder, filename)
+
+    if dir_filename not in glob.glob(f'{output_folder}/*.html'):
+        RACE_INFERER.draw(race_name, comparison_race_name, segment_type, dir_filename)
+
+    json_dict = {'filename': f"/static/output/{filename}"}
+
+    return jsonify(json_dict)
+
+
+@app.route('/race-detail/comparison_table', methods=['POST'])
+def get_comparison_table():
+    race_name = request.form['race_name']
+    comparison_race_name = request.form['comparison_race_name']
+    segment_type = request.form['segment_type']
+
+    deniv_segment, length_segment, density_segment = RACE_INFERER.get_best_segment(race_name)
+    segments = deniv_segment + length_segment + density_segment
+
+    segment = None
+
+    for segment_tuple in segments:
+        if segment_tuple[0] == comparison_race_name and segment_tuple[1].segment_type == segment_type:
+            segment = segment_tuple[1]
+            break
+
+    json_dict = segment.get_statistics()
+
+    return jsonify(json_dict)
+
+
+@app.route('/race-detail/comparison_graphs', methods=['POST'])
+def get_comparison_graphs():
+    race_name = request.form['race_name']
+    comparison_race_name = request.form['comparison_race_name']
+    segment_type = request.form['segment_type']
+
+    deniv_segment, length_segment, density_segment = RACE_INFERER.get_best_segment(race_name)
+    segments = deniv_segment + length_segment + density_segment
+
+    segment = None
+
+    for segment_tuple in segments:
+        if segment_tuple[0] == comparison_race_name and segment_tuple[1].segment_type == segment_type:
+            segment = segment_tuple[1]
+            break
+
+    start = segment.points1[0].distance / 1000
+    stop = segment.points1[-1].distance / 1000
+
+    json_dict = {'start': start, 'stop': stop}
 
     return jsonify(json_dict)
 
--- viewer/static/main.js
+++ viewer/static/main.js
@@ -1,3 +1,5 @@
+var GLOBAL_DATA_STATS = {};
+
 function pop(elem, data) {
     let array = "";
 
@@ -27,23 +29,80 @@ function filter_arrays(y_array, x_array) {
     return array;
 }
 
-function plot_graph(container, f_append, name, unit, y_array, x_array, color) {
+function plot_graph(container, f_append, name, unit, y_array, x_array, color, start, stop) {
 
     let data = filter_arrays(y_array, x_array);
 
-    let config = {
-        type: 'line',
-        data: {
-            labels: data[0],
-            datasets: [{
-                label: name,
+    let graph_data = {
+        labels: data[0],
+        datasets: [{
+            label: name,
+            fill: true,
+            backgroundColor: color,
+            borderColor: color,
+            pointRadius: 0,
+            data: data[1]
+        }]
+    };
+
+    if (start > 0) {
+        let grey = window.chartColors.grey;
+        let data_left = [];
+        let data_segment = [];
+        let data_right = [];
+
+        for (let i = 0; i < data[0].length; i++) {
+            let lbl = data[0][i];
+
+            if (lbl < start) {
+                data_left.push(data[1][i]);
+                data_right.push(NaN);
+                data_segment.push(NaN);
+            } else if (lbl > stop) {
+                data_right.push(data[1][i]);
+                data_left.push(NaN);
+                data_segment.push(NaN);
+            } else {
+                data_segment.push(data[1][i]);
+                data_left.push(NaN);
+                data_right.push(NaN);
+            }
+        }
+
+        let datasets = [
+            {
+                label: "Race's " + name,
+                fill: true,
+                backgroundColor: grey,
+                borderColor: grey,
+                pointRadius: 0,
+                data: data_left
+            },
+            {
+                label: "Segment's " + name,
                 fill: true,
                 backgroundColor: color,
                 borderColor: color,
                 pointRadius: 0,
-                data: data[1],
-            }]
-        },
+                data: data_segment
+            },
+            {
+                label: "Race's " + name,
+                fill: true,
+                backgroundColor: grey,
+                borderColor: grey,
+                pointRadius: 0,
+                data: data_right
+            }
+        ];
+
+        graph_data["datasets"] = datasets;
+    }
+
+
+    let config = {
+        type: 'line',
+        data: graph_data,
         options: {
             responsive: true,
             title: {
@@ -74,7 +133,24 @@ function plot_graph(container, f_append, name, unit, y_array, x_array, color) {
                     }
                 }]
             }
-        }
+        },
+        plugins: [{
+            beforeRender: function (x, options) {
+                for (let i = 0; i < x.data.datasets.length; i++) {
+                    var c = x.chart;
+                    var dataset = x.data.datasets[i];
+
+                    let color = x.data.datasets[i].backgroundColor;
+
+                    var gradientFill = c.ctx.createLinearGradient(0, 0, 0, c.height);
+                    gradientFill.addColorStop(0, color);
+                    gradientFill.addColorStop(1, 'white');
+
+                    var model = x.data.datasets[i]._meta[Object.keys(dataset._meta)[0]].dataset._model;
+                    model.backgroundColor = gradientFill;
+                }
+            }
+        }]
     };
 
     let canvas = $('<canvas>').attr("width", "450").attr("height", "450");
@@ -84,10 +160,49 @@ function plot_graph(container, f_append, name, unit, y_array, x_array, color) {
     window.myLine = new Chart(canvas, config);
 }
 
-function call_stats(container, f_append, table_class) {
+function add_table_stats(data, container, table_class) {
 
     container.find(".table-stats").empty();
+
+    let table = $('<table>').addClass('table');
+    let thead = $('<thead>').addClass('thead-dark');
+    let tr = $('<tr>').append($('<th>').attr('scope', 'col').text("Stats"))
+        .append($('<th>').attr('scope', 'col').text("Values"));
+    let tbody = $('<tbody>');
+
+    thead.append(tr);
+    table.append(thead);
+
+    $.each(data, function (i, item) {
+
+        let row = $('<tr>');
+        let title = $('<td>').text(i);
+        let content = $('<td>').text(item);
+
+        row.append(title).append(content);
+
+        tbody.append(row);
+    });
+
+    table.append(tbody);
+
+    let row_table = container.find('.table-stats');
+    let col = $('<div>').addClass(table_class);
+
+    col.append(table);
+    row_table.append(col);
+}
+
+function add_graphs_stats(container, f_append, distances, speeds, bpms, heights, start = -1, stop = Number.MAX_SAFE_INTEGER) {
+
     container.find(".graphs-stats").empty();
+
+    plot_graph(container, f_append, "Speed", "(km/h)", speeds, distances, window.chartColors.red, start, stop);
+    plot_graph(container, f_append, "BPM", "", bpms, distances, window.chartColors.blue, start, stop);
+    plot_graph(container, f_append, "Height", "(m)", heights, distances, window.chartColors.green, start, stop);
+}
+
+function call_stats(container, f_append, table_class) {
 
     var url = "/race-detail/statistics";
     var FD = new FormData();
@@ -101,44 +216,198 @@ function call_stats(container, f_append, table_class) {
         contentType: false
     }).done(function (data) {
 
-        var distances = pop('distances', data);
-        var speeds = pop('speeds', data);
-        var bpms = pop('bpms', data);
-        var heights = pop('heights', data);
+        GLOBAL_DATA_STATS = JSON.parse(JSON.stringify(data)); //deep copy
 
+        let distances = pop('distances', data);
+        let speeds = pop('speeds', data);
+        let bpms = pop('bpms', data);
+        let heights = pop('heights', data);
 
-        var table = $('<table>').addClass('table');
-        var thead = $('<thead>').addClass('thead-dark');
-        var tr = $('<tr>').append($('<th>').attr('scope', 'col').text("Stats"))
-            .append($('<th>').attr('scope', 'col').text("Values"));
-        var tbody = $('<tbody>');
+        add_table_stats(data, container, table_class);
 
-        thead.append(tr);
-        table.append(thead);
+        add_graphs_stats(container, f_append, distances, speeds, bpms, heights);
+    });
+}
 
-        $.each(data, function (i, item) {
+function add_segments_list(data) {
+    let container = $('#segments-comparison');
 
-            let row = $('<tr>');
-            let title = $('<td>').text(i);
-            let content = $('<td>').text(item);
+    container.empty();
 
-            row.append(title).append(content);
+    let row = $('<div>').addClass("row h-100 justify-content-center");
+    let col_button = $('<div>').addClass("col-2");
+    let col_select = $('<div>').addClass("col-10");
+    let button = $('<button>').addClass("btn btn-primary my-1").text("Compare");
+    let select = $('<select>').addClass("custom-select my-1 mr-sm-2").attr("id", "select-segments");
 
-            tbody.append(row);
+    button.on("click", function () {
+        call_comparison_visualisation();
+        call_comparison_table();
+        call_comparison_graphs();
+    });
+
+    $.each(data, function (i, item) {
+        let comparison_race = item[0];
+        let segment_type = item[1];
+
+        let option = $('<option>').attr("value", comparison_race + "/" + segment_type)
+            .text(comparison_race + "/" + segment_type);
+
+        select.append(option);
+    });
+
+    col_button.append(button);
+    col_select.append(select);
+    row.append(col_button);
+    row.append(col_select);
+    row.css("margin-top", "50%");
+    container.append(row);
+}
+
+function call_comparison_segments() {
+
+    var url = "/race-detail/comparison_segments";
+    var FD = new FormData();
+    FD.append("race_name", race_name);
+
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: FD,
+        processData: false,
+        contentType: false
+    }).done(function (data) {
+        add_segments_list(data);
+    });
+}
+
+function call_comparison_graphs() {
+
+    let selected_segment = $("#select-segments option:selected").text();
+    selected_segment = selected_segment.split('/');
+
+    let comparison_race_name = selected_segment[0];
+    let segment_type = selected_segment[1];
+
+    var url = "/race-detail/comparison_graphs";
+    var FD = new FormData();
+    FD.append("race_name", race_name);
+    FD.append("comparison_race_name", comparison_race_name);
+    FD.append("segment_type", segment_type);
+
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: FD,
+        processData: false,
+        contentType: false
+    }).done(function (data) {
+        var container = $("#container-comparison");
+
+        function f_append(canvas) {
+            let row = $('<div>').addClass("row");
+            let col = $('<div>').addClass("col-8");
+            col.css("margin-left", "8%");
+            col.append(canvas);
+            row.append(col);
+            container.find(".graphs-stats").append(row);
+        }
+
+        let start = data['start'];
+        let stop = data['stop'];
+
+        let distances = pop('distances', GLOBAL_DATA_STATS);
+        let speeds = pop('speeds', GLOBAL_DATA_STATS);
+        let bpms = pop('bpms', GLOBAL_DATA_STATS);
+        let heights = pop('heights', GLOBAL_DATA_STATS);
+
+        add_graphs_stats(container, f_append, distances, speeds, bpms, heights, start, stop)
+    });
+}
+
+function call_comparison_table() {
+
+    let selected_segment = $("#select-segments option:selected").text();
+    selected_segment = selected_segment.split('/');
+
+    let comparison_race_name = selected_segment[0];
+    let segment_type = selected_segment[1];
+
+    var url = "/race-detail/comparison_table";
+    var FD = new FormData();
+    FD.append("race_name", race_name);
+    FD.append("comparison_race_name", comparison_race_name);
+    FD.append("segment_type", segment_type);
+
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: FD,
+        processData: false,
+        contentType: false
+    }).done(function (data) {
+        let container = $("#container-comparison");
+        add_table_stats(data, container, "col-11");
+    });
+}
+
+function call_comparison_visualisation() {
+
+    let selected_segment = $("#select-segments option:selected").text();
+    selected_segment = selected_segment.split('/');
+
+    let comparison_race_name = selected_segment[0];
+    let segment_type = selected_segment[1];
+
+    var url = "/race-detail/comparison_visualisation";
+    var FD = new FormData();
+    FD.append("race_name", race_name);
+    FD.append("comparison_race_name", comparison_race_name);
+    FD.append("segment_type", segment_type);
+
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: FD,
+        processData: false,
+        contentType: false
+    }).done(function (data) {
+
+        let container = $('#segments-comparison');
+        container.empty();
+
+        let return_button = $('<button>').addClass("btn btn-primary my-1").text("Return");
+
+        return_button.on("click", function () {
+            call_comparison_segments();
+            call_comparison_stats();
         });
 
-        table.append(tbody);
+        let object = $('<object>')
+            .attr('type', "text/html")
+            .attr('width', "650")
+            .attr("height", "650")
+            .attr('data', data['filename']);
 
-        let row_table = container.find('.table-stats');
-        let col = $('<div>').addClass(table_class);
-
-        col.append(table);
-        row_table.append(col);
-
-        plot_graph(container, f_append, "Speed", "(km/h)", speeds, distances, window.chartColors.red);
-        plot_graph(container, f_append, "BPM", "", bpms, distances, window.chartColors.blue);
-        plot_graph(container, f_append, "Height", "(m)", heights, distances, window.chartColors.green);
+        container.append(return_button);
+        container.append(object);
     });
+}
+
+function call_comparison_stats() {
+    let container = $("#container-comparison");
+    container.css("display", "flex");
+
+    function f_append(canvas) {
+        let row = $('<div>').addClass("row");
+        let col = $('<div>').addClass("col-8");
+        col.css("margin-left", "8%");
+        col.append(canvas);
+        row.append(col);
+        container.find(".graphs-stats").append(row);
+    }
+
+    call_stats(container, f_append, "col-11");
 }
 
 function show_stats() {
@@ -155,26 +424,15 @@ function show_stats() {
 }
 
 function show_comparison() {
-    var container = $("#container-comparison");
-    container.css("display", "flex");
-
-    function f_append(canvas) {
-        let row = $('<div>').addClass("row");
-        let col = $('<div>').addClass("col-8");
-        col.css("margin-left", "8%");
-        col.append(canvas);
-        row.append(col);
-        container.find(".graphs-stats").append(row);
-    }
-
-    call_stats(container, f_append, "col-10");
+    call_comparison_stats();
+    call_comparison_segments();
 }
 
 function show_visualisation() {
     $('#nav-visualisation').empty();
 
-    var url = "/race-detail/visualisation";
-    var FD = new FormData();
+    let url = "/race-detail/visualisation";
+    let FD = new FormData();
     FD.append("race_name", race_name);
 
     $.ajax({
@@ -184,7 +442,7 @@ function show_visualisation() {
         processData: false,
         contentType: false
     }).done(function (data) {
-        var object = $('<object>')
+        let object = $('<object>')
             .attr('type', "text/html")
             .attr('width', "750")
             .attr("height", "750")
